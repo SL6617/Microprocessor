c6fef6cfeba30b5dbe9cf250e4be63a987009aa7
SL6617 <SL6617@skbl-422-015.ph.ic.ac.uk>
2020-10-16 16:08:18 +0100

    counted up to FF and made D the input

--- main.ss
+++ main.ss
@@ -9,20 +9,16 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw   0xff		;all bits in
-	movwf   TRISD, A	;Port D all input
-	
 	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISB, A	    ; Port C all outputs
 	bra 	test
-loop:	movff 	0x06, LATC
-	incf 	0x06, 0, 0        ;original code: 0x06, W, A
+loop:	movff 	0x06, PORTB
+	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	;movf	PORTD, 0    ;read PORTD to W
-	movlw 	0xFF
+	movlw 	0x63
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
-	goto	0x0	    ;Re-run program from start
+	goto 	0x0		    ; Re-run program from start
 
 	end	main
--- nbproject/counter.s
+++ nbproject/counter.s
@@ -9,20 +9,18 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw   0xff		;all bits in
-	movwf   TRISD, A	;Port D all input
-	
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:	movff 	0x06, LATC
-	incf 	0x06, 0, 0        ;original code: 0x06, W, A
+	incf 	0x06, 1, 0        ;original code: 0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	;movf	PORTD, 0    ;read PORTD to W
-	movlw 	0xFF
+	movlw 	0x04
 	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto	0x0	    ;Re-run program from start
+	bra 	loop
+	
+	; Not yet finished goto start of loop again
+	;goto	0x0	    ;Re-run program from start
 
 	end	main
