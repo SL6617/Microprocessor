73a86bd1a04f5a4be3c0548122030fdbf2635621
sl6617 <sl6617@skbl-422-015.ph.ic.ac.uk>
2020-11-05 14:33:11 +0000

    TWO BYTEW MOROMORY GETTING VHIPS TO DO HTING

--- main.s
+++ main.s
@@ -3,10 +3,11 @@
 
 psect	code, abs
 start:	
-	;clrf	PORTF
-	;banksel	ANCON1
-	;movlw	0x1F
-	;movwf	ANCON1
+	
+	clrf    TRISD, A ; set port D as outut
+	banksel	ANCON1
+	movlw	0x1F
+	movwf	ANCON1
 	;movlw	0x0F
 	;movwf	ANCON
 	goto	main
@@ -22,7 +23,7 @@ write:
 	movwf   0x21	    ;register for delay literal
 	call	another_delay
 	bsf	PORTD,0,A    ;sets CP2 as high
-	setf	TRISE, A    ;TRISE all high
+	;setf	TRISE, A    ;TRISE all high
 	return
 write2:
 
@@ -30,28 +31,28 @@ write2:
 	bsf	PORTD, 2, A  ;CP1 high
 	bsf	PORTD, 3, A  ;OE*1 high
 	bcf	PORTD,2,A   ;sets CP1 as low
-	movlw   0xff	    ;literal to be written into memory chip 2
-	movwf   PORTE, A
+	movlw   0x07	    ;literal to be written into memory chip 2
+	movwf   LATE, A
 	movlw   0xff	    ;setting the delay literal
 	movwf   0x21	    ;register for delay literal
 	call	another_delay
 	bsf	PORTD,2,A    ;sets CP1 as high
-	setf	TRISE, A    ;PORTE as input
+	;setf	TRISE, A    ;PORTE as input
 	return
 read:	
-	setf	TRISE, A    ;PORTE as input	
-	clrf	TRISF, A    ;PORTC all output	
+	;setf	TRISE, A    ;PORTE as input	
+	clrf	TRISC, A    ;PORTC all output	
 	bcf	PORTD,1,A   ;OE2* set as low
 	call	another_delay
-	movff	PORTE, LATF, A ;reading memory output will be on PORTC
+	movff	PORTE, LATC, A ;reading memory output will be on PORTC
 	bsf	PORTD,1,A   ;sets OE2* as high
 	setf	TRISE, A 
 	return
 read2:	
-	setf	TRISE, A    ;TRISE all high	
-	clrf	TRISH, A    ;PORTH all output
+	;setf	TRISE, A    ;TRISE all high	
+	clrf	TRISJ, A    ;PORTH all output
 	bcf	PORTD,3,A   ;OE1* set as low
-	movff	PORTE, LATH, A ;reading memory output will be on PORTH
+	movff	LATE, LATJ, A ;reading memory output will be on PORTH
 	bsf	PORTD,3,A   ;sets OE1* as high
 	setf	TRISE, A 
 	return
@@ -63,12 +64,14 @@ another_delay:
 
  main:
 	
-	;call    write2
-	;call	read2	
+	call    write2
 	call    write
+	call	read2	
+	
 	call    read
-	
-	
+
+	;clrf PORTF, A
+	;setf	TRISE, A 
 
     	end     main 
     
