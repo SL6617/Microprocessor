03931c635769865a9ceaff7ec0b786feb502a518
sophi <sophi@DESKTOP-5L0BL52>
2020-11-05 15:45:58 +0000

    STARTING THE CODE FOR SERIAL

--- main.s
+++ main.s
@@ -2,205 +2,57 @@
 	#include <xc.inc>
 
 psect	code, abs
-start:	
-	
-	clrf    TRISD, A ; set port D as outut
-	banksel	ANCON1
-	movlw	0x1F
-	movwf	ANCON1
-	;movlw	0x0F
-	;movwf	ANCON
-	goto	main
+main: 
+    
+    clrf TRISC, A ; set leds at port C as output
+    call SPI_paralell_to_series
+    call shift_series_to_paralell
+    movlw   0xff	    ;setting the delay literal
+    movwf   0x21	    ;register for delay literal
+    call another_delay
+    
+    goto main
+    
+SPI_paralell_to_series:
+    call SPI_MasterInit; initialises and sets up SPT
+    movlw 0x08; paralell data to be coverted
+    call SPI_MasterTransmit; convert to serial 
+    call Wait_transmit
+    return
+   
+shift_series_to_paralell:
+    movlw 0x08, 1,0
+    movwf 0x32
+    call clock_it_in
+    
 
-write:
-	clrf	TRISE, A    ; enablePORTE output
-	bsf	PORTD, 0, A  ;CP2 high
-	bsf	PORTD, 1, A  ;OE*2 high
-	bcf	PORTD,0,A   ;sets CP2 as low
-	movlw   0x05	    ;literal to be written into memory chip 1
-	movwf   PORTE, A
-	movlw   0xff	    ;setting the delay literal
-	movwf   0x21	    ;register for delay literal
-	call	another_delay
-	bsf	PORTD,0,A    ;sets CP2 as high
-	;setf	TRISE, A    ;TRISE all high
-	return
-write2:
-
-	clrf	TRISE, A    ;PORTE as output
-	bsf	PORTD, 2, A  ;CP1 high
-	bsf	PORTD, 3, A  ;OE*1 high
-	bcf	PORTD,2,A   ;sets CP1 as low
-	movlw   0x07	    ;literal to be written into memory chip 2
-	movwf   LATE, A
-	movlw   0xff	    ;setting the delay literal
-	movwf   0x21	    ;register for delay literal
-	call	another_delay
-	bsf	PORTD,2,A    ;sets CP1 as high
-	;setf	TRISE, A    ;PORTE as input
-	return
-read:	
-	;setf	TRISE, A    ;PORTE as input	
-	clrf	TRISC, A    ;PORTC all output	
-	bcf	PORTD,1,A   ;OE2* set as low
-	call	another_delay
-	movff	PORTE, LATC, A ;reading memory output will be on PORTC
-	bsf	PORTD,1,A   ;sets OE2* as high
-	setf	TRISE, A 
-	return
-read2:	
-	;setf	TRISE, A    ;TRISE all high	
-	clrf	TRISJ, A    ;PORTH all output
-	bcf	PORTD,3,A   ;OE1* set as low
-	movff	LATE, LATJ, A ;reading memory output will be on PORTH
-	bsf	PORTD,3,A   ;sets OE1* as high
-	setf	TRISE, A 
-	return
+clock_it_in:
+    bCf PORTD,0
+    BsF PORTD, 0
+    decfsz 0x32, A ;0x32  adress to store number 8, so we clock in only 8 times
+    bra clock_it_in
+    return
+    
+    bra
+SPI_MasterInit: ; Set Clock edge to negative
+    bcf CKE ; CKE bit in SSP2STAT; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
+    movlw (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+    movwf SSP2CON1, A ; SDO2 output; SCK2 output
+    bcf TRISD, PORTD_SDO2_POSN, A ; SDO2 output
+    bcf TRISD, PORTD_SCK2_POSN, A ; SCK2 output
+    return
+    
+SPI_MasterTransmit: ; Start transmission of data (held in W)
+    movwf SSP2BUF, A ; write data to output buffer
+    return
+    
+Wait_Transmit: ; Wait for transmission to complete
+    btfss SSP2IF ; check interrupt flag to see if data has been sent
+    bra Wait_Transmit
+    bcf SSP2IF ; clear interrupt flag
+    return 
 
 another_delay:
-	decfsz	0x21, 1, 0
-	bra     another_delay
-	return
-
- main:
-	
-	call    write2
-	call    write
-	call	read2	
-	
-	call    read
-
-	;clrf PORTF, A
-	;setf	TRISE, A 
-
-    	end     main 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+    decfsz	0x21, 1, 0
+    bra     another_delay
+    return
