7a0d462602d805c2d16108fc729f51ad1ca22852
SL6617 <SL6617@skbl-422-015.ph.ic.ac.uk>
2020-11-06 14:27:46 +0000

    SIMPLE VERSION SERIAL TO PARALELL

--- main.s
+++ main.s
@@ -4,40 +4,8 @@
 psect	code, abs
 start:
     clrf TRISD, A		;PORTD output
-    setf TRISF, A		;PORTF input (output LEDs)
     call SPI_MasterInit	;initialises and sets up SPT
-
-
-main: 
-    call SPI_parallel_to_series
-    call shift_series_to_parallel
-    ;movlw   0xff		;setting the delay literal
-    ;movwf   0x21		;register for delay literal
-    ;call another_delay
-    ;movff PORTD, LATF, A
-    end main
-    
-SPI_parallel_to_series:
-    movlw 0x08			;parallel data to be coverted
-    call SPI_MasterTransmit ;convert to serial 
-    return
-   
-shift_series_to_parallel:
-    movlw   0x08		;count down from 8 so only 8 things clocked in
-    movwf   0x32			;address to put counter in
-    ;bsf    PORTD, 9, A		;set the MR as high so shit happens
-    call    clock_it_in		;should be on outputs now  i think lol
-    return			
-
-clock_it_in:
-    bcf	    PORTD, 6, A		;clock pulse port D low
-    movlw   0xff		;setting the delay literal
-    movwf   0x21
-    call    another_delay
-    bsf	    PORTD, 6, A		;clock pulse HIGH - data should be read on the rise of this.
-    decfsz  0x32, A		;address to store number 8, so we clock in only 8 times
-    bra	    clock_it_in
-    return
+    goto main
   
 SPI_MasterInit:			;Set Clock edge to negative
     bcf CKE ; CKE bit in SSP2STAT; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
@@ -60,3 +28,8 @@ another_delay:
     decfsz	0x21, 1, 0
     bra		another_delay
     return
+    
+main: 
+    movlw   0x19
+    call SPI_MasterTransmit ;convert to serial 
+    goto 0x0
