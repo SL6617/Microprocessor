cfe1dd301a21ae1f6620f2c4ca4e70388aaa631f
SL6617 <SL6617@skbl-422-015.ph.ic.ac.uk>
2020-11-10 15:21:50 +0000

    PROJECT 1 - code plan and delegation

--- main.s
+++ main.s
@@ -1,62 +1,66 @@
 	#include <pic18_chip_select.inc>
 	#include <xc.inc>
-
+	
+;DON'T FORGET RETURN AT END OF SUBROUTINES!!!!
 psect	code, abs
-start:
-    clrf TRISD, A		;PORTD output
-    setf TRISF, A		;PORTF input (output LEDs)
-    call SPI_MasterInit	;initialises and sets up SPT
+setup:
+    call setup
 
+main:
+    call is_human_there ; runs the detection check from the PIR then thermal sensor
+    call alarm_system_on ;turn on buzzer, voice message, flasing lights
+    call keycode_check
+    call mega_alarm
+     
+setup:;initialize then programme devices but don't turn on
+    light_set_up:
+    buzzer_set_up: - S
+    LCD_set_up:  -B
 
-main: 
-    call SPI_parallel_to_series
-    call shift_series_to_parallel
-    ;movlw   0xff		;setting the delay literal
-    ;movwf   0x21		;register for delay literal
-    ;call another_delay
-    ;movff PORTD, LATF, A
-    goto 0x0
+    keypad_set_up: -B
+    PIR_sensor_set_up:- S
+    thermal_sensor_set_up:-S
+    fruit_board_set_up: - B
+return ; returns to setup
+
+is_human_there:
+    check_PIR_status:
+	; is the voltage trigered?
+	; no - bra check_PIR_status
+	; yes - NOP
+    trigger_thermal_sensor:
+	; check for human warmth
+	; no - bra check PIR - status
+	; yes - return to main to trigger alarm
+ 
+
+alarm_system_on:
+    call LCD1: ; this will be LCD displaying 'enter code'
+    buzzer_on:
+    lights_on:
+    voice1_on: ; 'get out of my house'
     
-SPI_parallel_to_series:
-    movlw 0x63			;parallel data to be coverted
-    call SPI_MasterTransmit ;convert to serial 
-    return
-   
-shift_series_to_parallel:
-    movlw   0x08		;count down from 8 so only 8 things clocked in
-    movwf   0x32			;address to put counter in
-    ;bsf    PORTD, 9, A		;set the MR as high so shit happens
-    call    clock_it_in		;should be on outputs now  i think lol
-    return			
-
-clock_it_in:
-    bcf	    PORTD, 6, A		;clock pulse port D low
-    movlw   0xff		;setting the delay literal
-    movwf   0x21
-    call    another_delay
-    bsf	    PORTD, 6, A		;clock pulse HIGH - data should be read on the rise of this.
-    decfsz  0x32, A		;address to store number 8, so we clock in only 8 times
-    bra	    clock_it_in
-    return
+;person is trying to enter the keycode
   
-SPI_MasterInit:			;Set Clock edge to negative
-    bcf CKE ; CKE bit in SSP2STAT; MSSP enable; CKP=1; SPI master, clock=Fosc/64 (1MHz)
-    movlw (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
-    movwf SSP2CON1, A ; SDO2 output; SCK2 output
-    bcf TRISD, PORTD_SDO2_POSN, A ; SDO2 output
-    bcf TRISD, PORTD_SCK2_POSN, A ; SCK2 output
-    return
-    
-SPI_MasterTransmit:
-    ;Starts transmission of data (held in W)
-    movwf SSP2BUF, A				;write data to output buffer
-Wait_Transmit: ; Wait for transmission to complete
-    btfss SSP2IF ; check interrupt flag to see if data has been sent
-    bra Wait_Transmit
-    bcf SSP2IF ; clear interrupt flag
-    return 
+keycode_check:
+    ;set up counter register to 
+    ;tries to enter code
+    ;if correct call - disable_alarm
+    ;if wrong call LCD2: 'incorrect, try again'
+    ;increment counter (up to 3) up by one
+    ;if ocunter in register in 3 skip next line
+    ;bra keycode_check
+    ;return
+ 
+disable_alarm:
+;disables alarm and everything
+;end
+ 
+LCD1: ;'enter code'
+LCD2: ;incorrect try again
+LCD3: ;'system locked - calling police'
 
-another_delay:
-    decfsz	0x21, 1, 0
-    bra		another_delay
-    return
+mega_alarm:
+    mega_buzzer_on:
+    mega_lights_on:
+    voice2_on: ;'police being called'
